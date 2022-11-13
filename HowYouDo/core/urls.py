84bb05a65c00f55171ba9c362b9958430d8c6657
NecibeBusraUylas <busrauylas01@gmail.com>
2022-11-13 16:27:25 +0300

    Profile Settings Page

--- HowYouDo/core/urls.py
+++ HowYouDo/core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'), # home page
     path('signup', views.signup, name='signup'),  # sign up/ register page
     path('signin', views.signin, name='signin'),  # sign in page
-    path('logout', views.logout, name='logout')  # sign in page
+    path('logout', views.logout, name='logout'),  # sign in page
+    path('settings', views.settings, name='settings')  # profile settings page
 ]
--- HowYouDo/core/views.py
+++ HowYouDo/core/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib.auth.decorators import login_required
-from .models import Profile
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, auth
+from django.shortcuts import redirect, render
+
+from .models import Profile
 
 
 # Create your views here.
@@ -12,6 +13,8 @@ def index(request):
 
 def signup(request):
     if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
@@ -25,15 +28,17 @@ def signup(request):
                 return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
+                user.first_name = firstname
+                user.last_name = lastname
                 user.save()
                 # log user in and redirect to setting page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 # create a profile object for the new user
-                
-                # for sending user the profile model
                 user = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user,id_user=user.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
         else:
             messages.error(request, 'Password not matching!!!')
             return redirect('/signup')
@@ -55,7 +60,34 @@ def signin(request):
             return redirect('/')
     else:
         return render(request, 'signin.html')
-    
+
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user) # getting the profile object in which the user the currently logged in user
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileImage # if user didn't summit image it's just getting the default image 
+            about = request.POST['about']
+            location = request.POST['location']
+           
+            user_profile.profileImage = image
+            user_profile.about= about
+            user_profile.location = location
+            user_profile.save()
+            
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')  # if user summit image it's getting the image
+            about = request.POST['about']
+            location = request.POST['location']
+            
+            user_profile.profileImage = image
+            user_profile.about = about
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile}) # pass the profile object to settings page
